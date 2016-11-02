47f49a207753ad0b2433f2e7ecea6199a0dc5128
Raniro Coelho <errecme@gmail.com>
2016-11-02 21:49:20 -0200

    setup wikipedia API

--- development/js/main.js
+++ development/js/main.js
@@ -1,1 +1,19 @@
+var lang = 'pt';
+var keyword = 'Montreal';
+var numberOfEntries = 5;
 
+$('.random-btn').click(function(){
+   window.location.href='https://' + lang + '.wikipedia.org/wiki/Special:Random';
+});
+
+$.getJSON('https://' + lang + '.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + keyword + '&limit=' + numberOfEntries + '&callback=?', function(data) {
+
+  var numberOfResults = data[1].length;
+
+  console.log(numberOfResults);
+  console.log('https://' + lang + '.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + keyword + '&limit=' + numberOfEntries + '&callback=?');
+
+});
+
+  function SetResultsGrid(){
+  } // TODO
