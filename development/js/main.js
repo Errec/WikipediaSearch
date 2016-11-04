0a6da1a0cfaad2e5b069f6ab7daefea4fb833fbe
Raniro Coelho <errecme@gmail.com>
2016-11-04 13:17:07 -0200

    add append loop

--- development/js/main.js
+++ development/js/main.js
@@ -1,4 +1,4 @@
-var lang = 'pt';
+var lang = 'en';
 var keyword = 'Montreal';
 var numberOfEntries = 5;
 
@@ -7,6 +7,11 @@ $('.random-btn').click(function(){
 });
 
 $.getJSON('https://' + lang + '.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + keyword + '&limit=' + numberOfEntries + '&callback=?', function(data) {
+
+  for (var i = 0; i < data[1].length; i++) {
+    $('body').append('<h3>' + data[1][i] + '</h3>');
+    $('body').append('<p>' + data[2][i] + '</p>');
+  }
 
   var numberOfResults = data[1].length;
 
